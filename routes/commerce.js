43c7140602c7b7c0e3cc91d45d0a138811b298f5
Westindiess <gregoryalexiss@gmail.com>
2022-05-29 14:28:43 +0200

    commerce routes

---
+++ routes/commerce.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const CommerceCtrl = require("../controllers/commerce.js");
+
+router.get("/secteur/:id", CommerceCtrl.findCommerceByActivity);
+router.get("/location/:id", CommerceCtrl.findCommerceByCity);
+//router.get("/typeincity/:id", CommerceCtrl.findCommerceActivityInCity);
+
+module.exports = router;
