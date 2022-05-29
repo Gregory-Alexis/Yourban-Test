265b2c635a96dae8878ddfde5e68db348e856e89
Westindiess <gregoryalexiss@gmail.com>
2022-05-29 23:45:41 +0200

    file renamed

--- routes/etablissement.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const EtablissementCtrl = require("../controllers/etablissement.js");
-
-router.get("/city/:id", EtablissementCtrl.findEtablissement);
-//router.get("/location/:id", EtablissementCtrl);
-//router.get("/typeincity/:id", EtablissementCtrl);
-
-module.exports = router;
