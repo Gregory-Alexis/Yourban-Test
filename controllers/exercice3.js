b74ad1e07ba5630193dda33a720e152e4a340d89
Westindiess <gregoryalexiss@gmail.com>
2022-05-30 01:17:48 +0200

    add delete controllers exo 3

--- controllers/exercice3.js
+++ controllers/exercice3.js
@@ -4,26 +4,24 @@ const data = require("../MOCK_DATA.json");
 
 exports.findEtablissementByCity = (req, res) => {
   const { id } = req.params;
-
   // Récupère les donnée des établissement par ville
   try {
     const newData = data
       .filter((data) => {
         if (data.location.toLocaleLowerCase() === id) {
-          return data.toString();
+          return data;
         }
       })
-      .map((data) => data.etablissement.toString());
-    res.send(newData);
+      .map((data) => data.etablissement);
+    res.status(200).json(newData);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(400).json({ error });
   }
 };
 
 exports.findEtablissementBySectorAndCity = (req, res) => {
   // Récupère les donnée des établissement par secteur et ville
   const { sector, city } = req.params;
-
   try {
     const newData = data
       .filter((data) => {
@@ -36,15 +34,30 @@ exports.findEtablissementBySectorAndCity = (req, res) => {
       })
       .map((data) => data);
 
-    res.send(newData);
+    res.status(200).json(newData);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(400).json({ message: err.message });
   }
 };
 
-// Supression d'une donnée
-exports.deleteData = (req, res) => {
+// Supression de tout les établissement d'une ville
+exports.deleteAllEtablissementInCity = (req, res) => {
   const { id } = req.params;
-  const deleteData = data.filter((data) => data.id !== Number(id));
-  res.send(deleteData);
+  try {
+    const deleteData = data.filter((data) => data.location !== id);
+    res.status(200).json(deleteData);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+// Supression de tout les établissement d'un secteur d'activité
+exports.deleteAllEtablissementPerType = (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteData = data.filter((data) => data.etablissement_type !== id);
+    res.status(200).json(deleteData);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
