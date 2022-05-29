e8d09241f3f11ddfa44233b2b6dc7d3ad1960b71
Westindiess <gregoryalexiss@gmail.com>
2022-05-30 01:17:31 +0200

    add error handler & response status

--- controllers/exercice1.js
+++ controllers/exercice1.js
@@ -1,63 +1,84 @@
 let data = require("../MOCK_DATA.json");
 
+///// Exercice 1
+
 // Créer une nouvelle donnée
 exports.createData = (req, res) => {
-  const createData = req.body;
-  data.push(createData);
-  res.send(
-    `Le nouvelle établissement ${createData.etablissement} a bien été créé`
-  );
+  try {
+    const createData = req.body;
+    data.push(createData);
+    res.status(200).json({
+      message: `Le nouvelle établissement ${createData.etablissement} a bien été créé`,
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
-// Récupère les données
+// Récupère toute les données
 exports.getData = (req, res) => {
-  res.send(data);
+  try {
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 exports.findData = (req, res) => {
   const { id } = req.params;
-  /* 
+  try {
+    /* 
   la consigne demandait de recupérer un élément par son nom, j'ai supposé que c'était le nom de l'établissement
 
-  Etant donné que le nom des établissements était composé de plusieurs mots, pour faciliter la recherche, 
+  Etant donné que le nom des établissements sont composé de plusieurs mots, pour faciliter la recherche (url), 
   j'ai supprimé les "espaces", ",Inc." et mis le tout en minuscule 
   */
-  const findData = data.find(
-    (data) =>
-      data.etablissement
-        .split(" ")
-        .join("")
-        .replace(",Inc.", "")
-        .toLowerCase() === id
-  );
-
-  /*
-   Pour récupérer les éléments par leur id, j'aurai procédé comme ceci
+    const findData = data.find(
+      (data) =>
+        data.etablissement
+          .split(" ")
+          .join("")
+          .replace(",Inc.", "")
+          .toLowerCase() === id.toLowerCase()
+    );
+    /*
+   Pour récupérer les éléments par leur id, j'aurai procédé comme ceci:
    const findData = data.find((data) => data.id === Number(id));
  */
-  res.send(findData);
+    res.status(200).json(findData);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 // Supression d'une donnée
 exports.deleteData = (req, res) => {
   const { id } = req.params;
-  const deleteData = data.filter((data) => data.id !== Number(id));
-  res.send(deleteData);
+  try {
+    const deleteData = data.filter((data) => data.id !== Number(id));
+    res.status(200).json(deleteData);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 // Mettre à jour une donnée
 exports.updateSingleData = (req, res) => {
   const { id } = req.params;
-  const updateData = data.find((data) => data.id === Number(id));
-  const { etablissement_type, etablissement, location, address, mail } =
-    req.body;
+  try {
+    const updateData = data.find((data) => data.id === Number(id));
+    const { etablissement_type, etablissement, location, address, mail } =
+      req.body;
 
-  // J'utilise la méthode PATCH pour mettre à jour un élément en particulier
-  if (etablissement_type) updateData.etablissement_type = etablissement_type;
-  if (etablissement) updateData.etablissement = etablissement;
-  if (location) updateData.location = location;
-  if (address) updateData.address = address;
-  if (mail) updateData.mail = mail;
+    // J'utilise la méthode PATCH pour mettre à jour un élément en particulier
+    if (etablissement_type) updateData.etablissement_type = etablissement_type;
+    if (etablissement) updateData.etablissement = etablissement;
+    if (location) updateData.location = location;
+    if (address) updateData.address = address;
+    if (mail) updateData.mail = mail;
 
-  res.send(updateData);
+    res.status(200).json(updateData);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
--- controllers/exercice2.js
+++ controllers/exercice2.js
@@ -1,27 +1,35 @@
 let data = require("../MOCK_DATA.json");
 
+///// Exercice 2
+
 exports.findCommerceByActivity = (req, res) => {
   const { id } = req.params;
 
-  // Récupère les donnée des établissement par type
-  const findCommerceByActivity = data.filter(
-    (data) => data.etablissement_type.toLowerCase() === id
-  );
-
-  res.send(
-    `Il y a ${findCommerceByActivity.length} établissement de type ${id}`
-  );
+  // Récupère les donnée des établissements par type
+  try {
+    const findCommerceByActivity = data.filter(
+      (data) => data.etablissement_type.toLowerCase() === id
+    );
+    // le récupère le nombre de type de commerce dans un secteur d'activité
+    res.status(200).json(findCommerceByActivity.length);
+  } catch (error) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 exports.findCommerceByCity = (req, res) => {
   const { id } = req.params;
 
   // Récupère les donnée des établissement par ville
-  const findCommerceByCity = data.filter(
-    (data) => data.location.toLowerCase() === id
-  );
-
-  res.send(`Il y a ${findCommerceByCity.length} établissement situé à ${id}`);
+  try {
+    const findCommerceByCity = data.filter(
+      (data) => data.location.toLowerCase() === id
+    );
+    // le récupère le nombre de commerce dans une ville donnée
+    res.status(200).json(findCommerceByCity.length);
+  } catch (error) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 /*exports.findCommerceActivityInCity = (req, res) => {
