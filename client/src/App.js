b64636c64aa0981965fb53ca47dbd7ba6e957e0e
Westindiess <gregoryalexiss@gmail.com>
2022-05-30 22:58:38 +0200

    App component refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import Tableau from "./components/Tableau";
+
 const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const result = await axios.get("http://localhost:8080/data");
+    setData(result.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Tableau data={data} fetchData={fetchData} />
+    </div>
+  );
 };
 
 export default App;
