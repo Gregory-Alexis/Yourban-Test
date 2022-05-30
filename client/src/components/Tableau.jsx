b40559e9fce83a82269530f4bfdaf73a9873e1ab
Westindiess <gregoryalexiss@gmail.com>
2022-05-30 23:00:06 +0200

    new Tableau component created

---
+++ client/src/components/Tableau.jsx
@@ -0,0 +1,74 @@
+import {
+  Center,
+  HStack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+import DeleteButton from "./DeleteButton";
+
+const Tableau = ({ data, fetchData }) => {
+  return (
+    <TableContainer>
+      <Table>
+        <Thead>
+          <Tr>
+            <HStack>
+              <Th flex="1">
+                <Center>Commerce</Center>
+              </Th>
+              <Th flex="1">
+                <Center>Etablissement</Center>
+              </Th>
+              <Th flex="1">
+                <Center>Location</Center>
+              </Th>
+              <Th flex="1">
+                <Center>Address</Center>
+              </Th>
+              <Th flex="1">
+                <Center>Mail</Center>
+              </Th>
+              <Th>
+                <Center>Action </Center>
+              </Th>
+            </HStack>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            {data.slice(0, 17).map((el) => (
+              <HStack w="100%" key={el.id}>
+                <Td flex="1" borderRight="1px">
+                  {el.etablissement_type}
+                </Td>
+                <Td flex="1" borderRight="1px">
+                  {el.etablissement}
+                </Td>
+                <Td flex="1" borderRight="1px">
+                  {el.location}
+                </Td>
+                <Td flex="1" borderRight="1px">
+                  {el.address}
+                </Td>
+                <Td flex="1" borderRight="1px">
+                  {el.mail}
+                </Td>
+                <Center>
+                  <DeleteButton pr="5" id={el.id} fetchData={fetchData} />
+                </Center>
+              </HStack>
+            ))}
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Tableau;
