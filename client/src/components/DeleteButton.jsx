cd4d102868403e0a8bf6bf7d2a4c5fc2dcdc5c27
Westindiess <gregoryalexiss@gmail.com>
2022-05-30 22:59:54 +0200

    add function to re-render when delete request

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -1,22 +1,22 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import axios from "axios";
 
-const DeleteButton = ({ id }) => {
+const DeleteButton = ({ id, fetchData }) => {
   const deleteHandler = async (id) => {
     await axios.delete(`http://localhost:8080/data/${id}`);
+    fetchData();
   };
 
   return (
-    <Box>
-      <Button
-        colorScheme="teal"
-        size="md"
-        mr="2"
-        onClick={() => deleteHandler(id)}
-      >
-        Suprrimer
-      </Button>
-    </Box>
+    <Button
+      colorScheme="teal"
+      mr={2}
+      size="sm"
+      borderBottom="2px solid black"
+      onClick={() => deleteHandler(id)}
+    >
+      Suprrimer
+    </Button>
   );
 };
 
