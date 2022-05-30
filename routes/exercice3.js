3b4866e5e92d7ab96e475056ec250e833106a077
Westindiess <gregoryalexiss@gmail.com>
2022-05-30 02:07:53 +0200

    refactor controllers name exo3

--- routes/exercice3.js
+++ routes/exercice3.js
@@ -2,18 +2,9 @@ const express = require("express");
 const router = express.Router();
 const EtablissementCtrl = require("../controllers/exercice3.js");
 
-router.get("/city/:id", EtablissementCtrl.findEtablissementByCity);
-router.get(
-  "/:sector/:city",
-  EtablissementCtrl.findEtablissementBySectorAndCity
-);
-router.delete(
-  "/delete/city/id:",
-  EtablissementCtrl.deleteAllEtablissementInCity
-);
-router.delete(
-  "/delete/sector/:id",
-  EtablissementCtrl.deleteAllEtablissementPerType
-);
+router.get("/city/:id", EtablissementCtrl.findByCity);
+router.get("/:sector/:city", EtablissementCtrl.findBySectorAndCity);
+router.delete("/delete/city/:id", EtablissementCtrl.deleteAllInCity);
+router.delete("/delete/sector/:id", EtablissementCtrl.deleteAllPerType);
 
 module.exports = router;
