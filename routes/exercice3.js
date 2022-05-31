9d241bb6b01520e2f0a507393744e181c7a86c1f
Westindiess <gregoryalexiss@gmail.com>
2022-05-31 15:24:09 +0200

    delete routes renamed

--- routes/exercice3.js
+++ routes/exercice3.js
@@ -4,7 +4,7 @@ const EtablissementCtrl = require("../controllers/exercice3.js");
 
 router.get("/:city", EtablissementCtrl.findByCity);
 router.get("/:sector/:city", EtablissementCtrl.findBySectorAndCity);
-router.delete("/delete/:city", EtablissementCtrl.deleteAllInCity);
-router.delete("/delete/:sector", EtablissementCtrl.deleteAllPerType);
+router.delete("/location/:city", EtablissementCtrl.deleteAllInCity);
+router.delete("/commerce/:sector", EtablissementCtrl.deleteAllPerType);
 
 module.exports = router;
