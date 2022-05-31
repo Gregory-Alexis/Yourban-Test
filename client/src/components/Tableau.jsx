961f7a3003839f550914f8390e5df8774d479520
Westindiess <gregoryalexiss@gmail.com>
2022-05-31 16:13:27 +0200

    remove Chrakra component

--- client/src/components/Tableau.jsx
+++ client/src/components/Tableau.jsx
@@ -1,6 +1,5 @@
 import {
   Center,
-  HStack,
   Table,
   TableContainer,
   Tbody,
@@ -18,53 +17,48 @@ const Tableau = ({ data, fetchData }) => {
       <Table>
         <Thead>
           <Tr>
-            <HStack>
-              <Th flex="1">
-                <Center>Commerce</Center>
-              </Th>
-              <Th flex="1">
-                <Center>Etablissement</Center>
-              </Th>
-              <Th flex="1">
-                <Center>Location</Center>
-              </Th>
-              <Th flex="1">
-                <Center>Address</Center>
-              </Th>
-              <Th flex="1">
-                <Center>Mail</Center>
-              </Th>
-              <Th>
-                <Center>Action </Center>
-              </Th>
-            </HStack>
+            <Th flex="1">
+              <Center>Commerce</Center>
+            </Th>
+            <Th flex="1">
+              <Center>Etablissement</Center>
+            </Th>
+            <Th flex="1">
+              <Center>Location</Center>
+            </Th>
+            <Th flex="1">
+              <Center>Address</Center>
+            </Th>
+            <Th flex="1">
+              <Center>Mail</Center>
+            </Th>
+            <Th>
+              <Center>Action </Center>
+            </Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            {data.slice(0, 17).map((el) => (
-              <HStack w="100%" key={el.id}>
-                <Td flex="1" borderRight="1px">
-                  {el.etablissement_type}
-                </Td>
-                <Td flex="1" borderRight="1px">
-                  {el.etablissement}
-                </Td>
-                <Td flex="1" borderRight="1px">
-                  {el.location}
-                </Td>
-                <Td flex="1" borderRight="1px">
-                  {el.address}
-                </Td>
-                <Td flex="1" borderRight="1px">
-                  {el.mail}
-                </Td>
-                <Center>
-                  <DeleteButton pr="5" id={el.id} fetchData={fetchData} />
-                </Center>
-              </HStack>
-            ))}
-          </Tr>
+          {data.slice(0, 17).map((el) => (
+            <Tr key={el.id}>
+              <Td flex="1" borderRight="1px">
+                {el.etablissement_type}
+              </Td>
+              <Td flex="1" borderRight="1px">
+                {el.etablissement}
+              </Td>
+              <Td flex="1" borderRight="1px">
+                {el.location}
+              </Td>
+              <Td flex="1" borderRight="1px">
+                {el.address}
+              </Td>
+              <Td flex="1" borderRight="1px">
+                {el.mail}
+              </Td>
+
+              <DeleteButton id={el.id} fetchData={fetchData} />
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </TableContainer>
