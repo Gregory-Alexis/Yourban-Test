70c32c9c0e33dcc971c7b8d0f129d75e4be31342
Westindiess <gregoryalexiss@gmail.com>
2022-05-31 02:23:06 +0200

    route refactoring

--- routes/exercice2.js
+++ routes/exercice2.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const CommerceCtrl = require("../controllers/exercice2.js");
 
-router.get("/secteur/:id", CommerceCtrl.findCommerceByActivity);
-router.get("/location/:id", CommerceCtrl.findCommerceByCity);
+router.get("/secteur/:activity", CommerceCtrl.findCommerceByActivity);
+router.get("/location/:city", CommerceCtrl.findCommerceByCity);
 router.get("/:sector/:city", CommerceCtrl.findCommerceActivityInCity);
 
 module.exports = router;
--- routes/exercice3.js
+++ routes/exercice3.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const EtablissementCtrl = require("../controllers/exercice3.js");
 
-router.get("/city/:id", EtablissementCtrl.findByCity);
+router.get("/:city", EtablissementCtrl.findByCity);
 router.get("/:sector/:city", EtablissementCtrl.findBySectorAndCity);
-router.delete("/delete/city/:id", EtablissementCtrl.deleteAllInCity);
-router.delete("/delete/sector/:id", EtablissementCtrl.deleteAllPerType);
+router.delete("/delete/:city", EtablissementCtrl.deleteAllInCity);
+router.delete("/delete/:sector", EtablissementCtrl.deleteAllPerType);
 
 module.exports = router;
